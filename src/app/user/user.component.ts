deb8028874ea036539bd80a86bf14a5c9c0264f4
Wambirul <wambirulorraine@gmail.com>
2021-06-11 02:01:00 +0300

    Create the user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { UserService } from './../user.service';
+import { User } from './../user';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  users!: User;
+
+  constructor(public myservice:UserService) { }
+
+  ngOnInit(): void {
+  }
+
+}
