2df1cb6d7d1c166f1600942a9a9e60880a73589a
Wambirul <wambirulorraine@gmail.com>
2021-06-11 01:52:32 +0300

    Create the user class blueprint

---
+++ src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+  static result: any;
+  constructor(public url:string, public login:string, public html_url:string, public public_repos:number, public followers:numbers, public following: number, public avatar_url:string, public created_at:Date){
+
+  }
+}
