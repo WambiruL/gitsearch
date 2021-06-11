a66b9a3482d5edb8d06f52a3d9e5352427a93155
Wambirul <wambirulorraine@gmail.com>
2021-06-11 03:20:22 +0300

    define the repoService

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,3 +1,4 @@
+import { Repo } from './../repo';
 import { UserService } from './../user.service';
 import { User } from './../user';
 import { Component, OnInit } from '@angular/core';
@@ -9,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   users!: User;
+  repos!:Repo;
 
-  constructor(public myservice:UserService) { }
+  constructor(public myservice:UserService, private reposervice: UserService) { }
+
+  searchs(searchName){
+    this.myservice.searchUser(searchName).then(
+      (success)=>{
+        this.users=this.myservice.foundUser;
+      },
+      (error)=>{
+        console.log(error)
+      }
+    );
+    this.reposervice.getRepos(searchName).then(
+      (results)=>{
+        this.repo =this.reposervice.allRepos
+        console.log(this.repos);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit(): void {
+    this.searchs('WambiruL')
   }
 
 }
